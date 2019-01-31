b92d364186c1047b4fd780e802a3bb5cf0a68b7c
julesl97_ChromeBook <jules.lagarde@gmail.com>
2019-01-31 12:39:20 +0100

    fixup! split source code

--- 2019/training/main.py
+++ 2019/training/main.py
@@ -4,44 +4,6 @@ import sys
 from pizza import Pizza, PizzaPart
 from logic import createsParts
 
-class Cell:
-    def __init__(self, x, y):
-        self.x=x
-        self.y=y
-
-
-
-def createsParts(pizza,parts):
-    print("createParts...")
-    #tab initialisation 
-    tab=[]
-    for x in range(pizza.w):
-        tab.append([])
-        for y in range(pizza.h):
-            tab[x].append(Cell(x,y))
-    #stats
-    tomatoes=0
-    mushrooms=0
-    for col in pizza.tab:
-        for c in col:
-            if c=='T':
-                tomatoes+=1
-            elif c=='M':
-                mushrooms+=1
-            else:
-                print("error:"+c)
-    print("T:"+str(tomatoes))
-    print("M:"+str(mushrooms))
-
-    #search
-    groupId=0
-    for col in tab:
-        for cell in col:
-            cell.groupId=groupId
-
-#   parts.append(PizzaPart(0,0,2,1))
-#   parts.append(PizzaPart(0,2,2,2))
-#   parts.append(PizzaPart(0,3,2,4))
 
 def writeFile(filename, parts):
     file = open(filename, "w")
