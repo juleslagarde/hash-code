debb39ca1bdc04516589ce7d47b41f130afc4400
lagarde <jlagarde@enssat.fr>
2019-02-03 01:33:56 +0100

    writing correction

--- 2019/training/main.py
+++ 2019/training/main.py
@@ -7,16 +7,21 @@ from logic import *
 
 def writeFile(filename, parts):
     file = open(filename, "w")
-    out = str(parts.length) + "\n"
+    out = str(len(parts)) + "\n"
     for part in parts:
         out += str(part) + "\n"
-    print(filename)
+    file.write(out)
+    print(filename+" written")
 
 
 def main(argv):
+    if(len(argv)!=3):
+        print("usage : python3 main.py <input> <output>")
+        exit()
     parts = {}
     pizza = readFile(argv[1])
     createsParts(pizza, parts)
+    writeFile(argv[2], parts.values())
     for p in parts:
         print(" " + str(parts[p]))
 
