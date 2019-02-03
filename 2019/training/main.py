46581a21fabd7005c3ba25b32366d0f40e9386cd
lagarde <jlagarde@enssat.fr>
2019-02-03 01:13:30 +0100

    first solution

--- 2019/training/main.py
+++ 2019/training/main.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python
 
 import sys
-from pizza import Pizza, PizzaPart
-from logic import createsParts
+from pizza import *
+from logic import *
 
 
 def writeFile(filename, parts):
     file = open(filename, "w")
-    out = str(parts.length)+"\n"
+    out = str(parts.length) + "\n"
     for part in parts:
-        out+=str(part)+"\n"
+        out += str(part) + "\n"
     print(filename)
 
+
 def main(argv):
-    parts = []
-    pizza = Pizza.readFile(argv[1])
-    createsParts(pizza,parts)
+    parts = {}
+    pizza = readFile(argv[1])
+    createsParts(pizza, parts)
     for p in parts:
-        print(" "+str(p))
+        print(" " + str(parts[p]))
+
 
 if __name__ == "__main__":
-    main(sys.argv);
-
+    main(sys.argv)
