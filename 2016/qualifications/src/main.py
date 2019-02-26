140d8567f4bb50a554836f8828de98c13e8d2da7
Jules Lagarde <jules.lagarde@gmail.com>
2019-02-26 16:47:05 +0100

    first idea

--- 2016/qualifications/src/main.py
+++ 2016/qualifications/src/main.py
@@ -8,3 +8,26 @@ FILES = [
 file_index = 0
 in_file = IN_FOLDER + FILES[file_index] + ".in"
 out_file = OUT_FOLDER + FILES[file_index] + ".out"
+
+
+
+
+
+
+
+
+
+
+def solve(simulation):
+    t = 0
+    while t < simulation.T:
+        for d in simulation.drones:
+            bestScore = 0
+            bestOrder = 0
+            for o in simulation.orders:
+                if d.scoreFor(o, t) > bestScore:
+                    bestScore = d.scoreFor(o, t)  #
+                    bestOrder = o
+            if bestOrder != 0:
+                d.deliver(bestOrder)
+        t += 1
