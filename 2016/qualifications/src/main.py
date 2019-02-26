5ffde00f34f02b26781f9b8744bc71b3bb14c8e2
Nicolas Ayala <nayala@enssat.fr>
2019-02-26 16:16:26 +0100

    added Input/Output template

---
+++ 2016/qualifications/src/main.py
@@ -0,0 +1,10 @@
+IN_FOLDER = "../in/"
+OUT_FOLDER = "../out/"
+FILES = [
+    "busy_day",
+    "mother_of_all_warehouses",
+    "redundancy",
+]
+file_index = 0
+in_file = IN_FOLDER + FILES[file_index] + ".in"
+out_file = OUT_FOLDER + FILES[file_index] + ".out"
