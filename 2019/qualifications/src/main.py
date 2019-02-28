88b22b4d86c375498145357a34e543d41d784285
Nicolas Ayala <nayala@enssat.fr>
2019-02-28 18:52:32 +0100

    Added input files

--- 2019/qualifications/src/main.py
+++ 2019/qualifications/src/main.py
@@ -1,8 +1,12 @@
 IN_FOLDER = "../in/"
 OUT_FOLDER = "../out/"
 FILES = [
-    "example",
+    "a_example",
+    "b_lovely_landscapes"
+    "c_memorable_moments"
+    "d_pet_pictures"
+    "e_shiny_selfies"
 ]
 file_index = 0
-in_file = IN_FOLDER + FILES[file_index] + ".in"
+in_file = IN_FOLDER + FILES[file_index] + ".txt"
 out_file = OUT_FOLDER + FILES[file_index] + ".out"
