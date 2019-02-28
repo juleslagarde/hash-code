7004576c89229f6a54e1079b303c69bbe411ef08
Nicolas Ayala <nayala@enssat.fr>
2019-02-28 19:00:33 +0100

    Added input

--- 2019/qualifications/src/main.py
+++ 2019/qualifications/src/main.py
@@ -10,3 +10,29 @@ FILES = [
 file_index = 0
 in_file = IN_FOLDER + FILES[file_index] + ".txt"
 out_file = OUT_FOLDER + FILES[file_index] + ".out"
+
+class Photo:
+    def __init__(self, id, file):
+        line = file.readline().split()
+        self.id = id
+        self.orientation = line[0]
+        self.tags_count = int(line[1])
+        self.tags = line[2:]
+
+    def __str__(self):
+        return str((self.id, self.orientation, self.tags))
+
+#===========================
+
+f = open(in_file, "r")
+photos_count = int(f.readline())
+photos = []
+slides = []
+
+for p in range(0, photos_count):
+    photos.append(Photo(p, f))
+
+#===========================
+
+for p in photos:
+    print(p)
