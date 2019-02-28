d9225da81477048d81c922ae5c7a5a47956e4ca7
Nicolas Ayala <nayala@enssat.fr>
2019-02-28 18:08:34 +0100

    Base hashcode stucture (2019)

---
+++ 2019/qualifications/src/main.py
@@ -0,0 +1,8 @@
+IN_FOLDER = "../in/"
+OUT_FOLDER = "../out/"
+FILES = [
+    "example",
+]
+file_index = 0
+in_file = IN_FOLDER + FILES[file_index] + ".in"
+out_file = OUT_FOLDER + FILES[file_index] + ".out"
